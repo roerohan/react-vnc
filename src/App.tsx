3d1b02464e5d4cfe9b4c3b16d7fa84d338ca9622
roerohan <roerohan@gmail.com>
2021-05-02 03:53:06 +0530

    fix: Add warning about ws:// URL

--- src/App.tsx
+++ src/App.tsx
@@ -24,10 +24,16 @@ function App() {
 
         <input type="text" onChange={({ target: { value } }) => {
           setInputUrl(value);
-        }} name="url" placeholder="ws://your-vnc-url" />
+        }} name="url" placeholder="wss://your-vnc-url" />
 
         <Spacer />
         <button onClick={() => setVncUrl(inputUrl)}>Go!</button>
+      </div>
+
+      <div style={{ opacity: 0.5, margin: '1rem' }}>
+        Since the site is loaded over HTTPS, only `wss://` URLs (SSL encrypted websockets URLs) are supported.
+        <br />
+        To test a `ws://` URL, clone the application and run it on http://localhost:3000, or <a href="https://experienceleague.adobe.com/docs/target/using/experiences/vec/troubleshoot-composer/mixed-content.html?lang=en#task_5448763B8DC941FD80F84041AEF0A14D">enable Mixed Content on your browser</a>.
       </div>
 
       <div style={{ margin: '1rem' }}>
