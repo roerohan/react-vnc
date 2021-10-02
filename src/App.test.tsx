ae59d4c0e8bba03efdd602b478c58b42d0028541
roerohan <roerohan@gmail.com>
2021-10-02 23:56:27 +0530

    test(tests): updated tests to succeed upon running npm t

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,14 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('contains input box for entering VNC URL', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const inputElement = screen.getByPlaceholderText(/wss:\/\/your-vnc-url/i);
+  expect(inputElement).toBeInTheDocument();
+});
+
+test('check if SSL disclaimer is present', () => {
+  render(<App />);
+  const disclaimerElement = screen.getByText(/Since the site is loaded over HTTPS, only `wss:\/\/` URLs \(SSL encrypted websockets URLs\) are supported./i);
+  expect(disclaimerElement).toBeInTheDocument();
 });
