9392ab3a2a06a88d77e1730823ee514309808658
Rohan Mukherjee <roerohan@gmail.com>
2022-02-19 19:50:48 +0530

    fix(forwardRef): forward connect(), disconnect() and connected from VncScreen (#20)
    * fix(forwardRef): forward connect(), disconnect() and connected from VncScreen
    
    * fix(build): fix failing builds
    
    * ci(build): run build:lib alongside build script

--- rollup.config.js
+++ rollup.config.js
@@ -5,9 +5,10 @@ const config = {
     input: pkg.source,
     output: [
         { file: pkg.main, format: 'cjs' },
+        { file: pkg.module, format: 'es' },
     ],
     plugins: [typescript({ tsconfig: './tsconfig.json', useTsconfigDeclarationDir: true })],
-    external: Object.keys(pkg.peerDependencies || {}),
+    external: Object.keys(pkg.peerDependencies ?? {}),
 };
 
 export default config;
