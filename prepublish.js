a956557f1eb6dbf620d13083c18f16d2b7da13f3
roerohan <roerohan@gmail.com>
2022-02-19 20:11:33 +0530

    fix(prepublish): remove unused library dependencies in prepublish script

--- prepublish.js
+++ prepublish.js
@@ -1,10 +1,19 @@
 const fs = require('fs');
 const pkg = require('./package.json');
 
+// NOTE(roerohan): remove dependencies that are not needed for the library.
 const {
 	react,
 	'react-scripts': reactScripts,
 	'react-dom': reactDom,
+	'@testing-library/jest-dom': testingLibraryjestDom,
+	'@testing-library/react': testingLibraryReact,
+	'@testing-library/user-event': testingLibraryUserEvent,
+	'@types/jest': typesJest,
+	'@types/node': typesNode,
+	'@types/react': typesReact,
+	'@types/react-dom': typesReactDom,
+	'web-vitals': webVitals,
 	...dependencies
 } = pkg.dependencies;
 
