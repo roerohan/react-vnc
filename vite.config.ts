7e2761e92cc1b4947e29eff4134977fe7978c4cd
Rohan Mukherjee <roerohan@gmail.com>
2025-02-25 04:28:57 +0530

    fix: builds and prepublish script

--- vite.config.ts
+++ vite.config.ts
@@ -4,21 +4,36 @@ import dts from 'vite-plugin-dts';
 import { resolve } from 'path';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    dts({ tsconfigPath: 'tsconfig.lib.json' }),
-  ],
-  server: {
-    port: 3000,
-  },
-  publicDir: 'public/',
-  build: {
-    lib: {
-      entry: resolve(__dirname, 'src', 'lib', 'index.tsx'),
-      name: 'ReactVnc',
-      formats: ['es', 'umd'],
+export default defineConfig(({ mode }) => {
+  if (mode === 'app') {
+    return {
+      plugins: [
+        react(),
+      ],
+      server: {
+        port: 3000,
+      },
+      publicDir: 'public/',
+      build: {
+        outDir: 'build',
+      }
+    }
+  }
+  return {
+    plugins: [
+      react(),
+      dts({ tsconfigPath: 'tsconfig.lib.json' }),
+    ],
+    server: {
+      port: 3000,
     },
-    copyPublicDir: false,
+    build: {
+      lib: {
+        entry: resolve(__dirname, 'src', 'lib', 'index.tsx'),
+        name: 'ReactVnc',
+        formats: ['es', 'umd'],
+      },
+      copyPublicDir: false,
+    }
   }
 });
